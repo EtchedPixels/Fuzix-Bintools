bdb0076b5e3d4745aa08289d61e39f646d75805e
Alan Cox <etchedpixels@gmail.com>
2024-05-11 22:13:52 +0100

    nova: add .ds

--- as6-nova.c
+++ as6-nova.c
@@ -23,6 +23,7 @@ SYM	sym[] = {
 	{	0,	"export",	TEXPORT,	XXXX	},
 	{	0,	".word",	TDEFW,		XXXX	},
 	{	0,	".blkw",	TDEFS,		XXXX	},
+	{	0,	".ds",		TDEFS,		XXXX	},
 	{	0,	".ascii",	TDEFM,		XXXX	},
 	{	0,	".org",		TORG,		XXXX	},
 	{	0,	".equ",		TEQU,		XXXX	},
