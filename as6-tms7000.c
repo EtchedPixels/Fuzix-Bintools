cec2a0a32f8abc1c8eda528181880d7cda3c6f1d
Alan Cox <etchedpixels@gmail.com>
2024-11-28 17:29:27 +0000

    tms7000: blkb is a 6800ism

--- as6-tms7000.c
+++ as6-tms7000.c
@@ -27,7 +27,7 @@ SYM	sym[] = {
 	{	0,	"export",	TEXPORT,	XXXX	},
 	{	0,	".byte",	TDEFB,		XXXX	},
 	{	0,	".word",	TDEFW,		XXXX	},
-	{	0,	".blkb",	TDEFS,		XXXX	},
+	{	0,	".ds",		TDEFS,		XXXX	},
 	{	0,	".ascii",	TDEFM,		XXXX	},
 	{	0,	".org",		TORG,		XXXX	},
 	{	0,	".equ",		TEQU,		XXXX	},
